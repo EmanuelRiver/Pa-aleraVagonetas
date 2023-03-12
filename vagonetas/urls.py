0b9fc4704bbea41d694c60293502bc9b744ff3f3
alee1rivero <alee123rivero@gmail.com>
2023-03-12 12:03:54 -0300

    Herencias de html hechas

--- vagonetas/urls.py
+++ vagonetas/urls.py
@@ -4,6 +4,9 @@ from vagonetas import views
 urlpatterns = [
     path('', views.Inicio,name='Inicio'),
     path('pañales',views.pañales,name='Pañales'),
+    path('indumentaria',views.indumentaria,name='Indumentaria'),
+    path('perfumeria',views.perfumeria,name='Perfumeria'),
+    path('accesorios',views.accesorios,name='Accesorios'),
    
 
 ]
--- vagonetas/views.py
+++ vagonetas/views.py
@@ -7,3 +7,12 @@ def Inicio(request):
 
 def pañales(request):
     return render(request,"vagonetas/pañales.html")
+
+def indumentaria(request):
+    return render(request,"vagonetas/indumentaria.html")
+
+def perfumeria(request):
+    return render(request,"vagonetas/perfumeria.html")
+
+def accesorios(request):
+    return render(request,"vagonetas/accesorios.html")
